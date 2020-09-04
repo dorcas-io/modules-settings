fd6447edbd53665b4d8e3dfa6f2174d9b4f0a61b
Ifeoluwa Olawoye <bolaji.olawoye@hostvillenigeria.org>
2020-09-04 15:18:46 +0100

    set default dorcas-modules publish tag

--- src/ModulesSettingsServiceProvider.php
+++ src/ModulesSettingsServiceProvider.php
@@ -11,10 +11,10 @@ class ModulesSettingsServiceProvider extends ServiceProvider {
 		$this->loadViewsFrom(__DIR__.'/resources/views', 'modules-settings');
 		$this->publishes([
 			__DIR__.'/config/modules-settings.php' => config_path('modules-settings.php'),
-		], 'config');
+		], 'dorcas-modules');
 		/*$this->publishes([
 			__DIR__.'/assets' => public_path('vendor/modules-settings')
-		], 'public');*/
+		], 'dorcas-modules');*/
 	}
 
 	public function register()
